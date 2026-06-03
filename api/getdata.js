0d70b12ce7a294e23369040582602b2b87251dde
satyamgupta1287-coder <satyamgupta1287@gmail.com>
2026-06-03 07:58:19 +0530

    Update API endpoint for number lookup

--- api/getdata.js
+++ api/getdata.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const url = `https://techvishalboss.com/api/v1/lookup.php?key=TVB_SGL_69F53CA4&service=number&number=${num}`;
+    const url = `https://pawan-osint.vercel.app/api?apikey=toxicadminn&number=${num}`;
     // 🔥 IMPORTANT: fetch ko safe banaya
     const response = await fetch(url, {
       method: "GET"
