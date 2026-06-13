1fa10eda0337bc863e159dd89eda60266819f1a1
satyamgupta1287-coder <satyamgupta1287@gmail.com>
2026-06-13 11:22:43 +0530

    Change API endpoint for fetching data

--- api/getdata.js
+++ api/getdata.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const url = `https://pawan-osint.vercel.app/api?apikey=toxicadminn&number=${num}`;
+    const url = `https://numberinfobysatyam.vercel.app/?apikey=satyam&number=${num}`;
     // 🔥 IMPORTANT: fetch ko safe banaya
     const response = await fetch(url, {
       method: "GET"
