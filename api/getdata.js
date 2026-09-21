c4f22019e5c433e9c1d05cf03cbf3d273273b1c8
Satyam Gupta <satyamgupta1287@gmail.com>
2026-09-21 16:20:06 +0530

    Update API URL for number information retrieval

--- api/getdata.js
+++ api/getdata.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const url = `https://numberinfobysatyam.vercel.app/?apikey=satyam&number=${num}`;
+    const url = `https://numberapi-tau.vercel.app/index.php?apikey=satyamm&number=${num}`;
     // 🔥 IMPORTANT: fetch ko safe banaya
     const response = await fetch(url, {
       method: "GET"
